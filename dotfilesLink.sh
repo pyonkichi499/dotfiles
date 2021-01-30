9136068604a10c699ea39078f23098abc46f55e3
Terauchi Hiroshi <terauchi.hiroshi@cloud-ace.jp>
2021-01-31 01:58:03 +0900

    re-order lines

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
