552aad598c7cb3609459bf7b50ab1be465233081
terauchi hiroshi <terauchi.hiroshi@cloud-ace.jp>
2024-04-17 23:42:27 +0900

    gitignore global を設定

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/.customize_environment ~/.customize_environment
