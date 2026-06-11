f844211bd1d0df45c6939b684a62069d5b579f9a
terauchi hiroshi <hrs499944@gmail.com>
2026-06-11 11:05:43 +0900

    feat: シンボリックリンクを作成するスクリプトにコメントを付与

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
+# shell
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# git
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitconfig-personal ~/.gitconfig-personal
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# LLM
+ln -sf ~/dotfiles/.claude ~/.claude
+ln -sf ~/dotfiles/.codex/AGENTS.md ~/.codex/AGENTS.md
+
+# etc
 ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/.customize_environment ~/.customize_environment
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.ssh/config ~/.ssh/config
-ln -sf ~/dotfiles/.codex/AGENTS.md ~/.codex/AGENTS.md
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
